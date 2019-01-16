a2cbe6bd737cc31fa465fa50d605d05dd0495feb
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-01-15 21:59:27 -0800

    this is third commit

--- .idea/src/main/Demo.java
+++ .idea/src/main/Demo.java
@@ -1,8 +1,21 @@
 package main;
 
-public class Demo {
-    public static void main(String args){
-        System.out.println("hello");
+import java.util.Arrays;
 
-    }
+public class Demo {
+    int[] list =new int[10];
+    public static void main(String args){
+
+        Demo l1=new Demo();
+        System.out.println(Arrays.toString(l1.list));
+        l1.insert(42);
+System.out.println(Arrays.toString(l1.list));
 }
+void insert(int value)
+{
+    for (int i = 0; i < list.length; i++) {
+if(list[i]==0)
+{
+list[i]=value;
+}
+    }}

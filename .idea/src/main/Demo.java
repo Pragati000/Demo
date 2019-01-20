dbafac77637cbb24a5f768fb9b088d929f02f58c
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-01-20 10:22:57 -0800

    this is all fresh

--- .idea/src/main/Demo.java
+++ .idea/src/main/Demo.java
@@ -3,19 +3,44 @@ package main;
 import java.util.Arrays;
 
 public class Demo {
-    int[] list =new int[10];
-    public static void main(String args){
+    int[] list = new int[10];
 
-        Demo l1=new Demo();
+    public static void main(String[] args)
+    {
+
+        Demo l1 = new Demo();
+
         System.out.println(Arrays.toString(l1.list));
-        l1.insert(42);
-System.out.println(Arrays.toString(l1.list));
+
+        for (int i = 0; i < 10; i++)
+        {
+            l1.insert(42 + i);
+        }
+
+        l1.remove(0);
+
+        System.out.println(Arrays.toString(l1.list));
+    }
+
+    void insert(int value)
+    {
+        for (int i = 0; i < list.length; i++)
+        {
+            if (list[i] == 0)
+            {
+                list[i] = value;
+                break;
+            }
+        }
+    }
+
+    void remove(int index)
+    {
+        for (int i = index; i < list.length - 1; i++)
+        {
+            list[i] = list[i + 1];
+        }
+        list[list.length - 1] = 0;
+    }
+
 }
-void insert(int value)
-{
-    for (int i = 0; i < list.length; i++) {
-if(list[i]==0)
-{
-list[i]=value;
-}
-    }}}
